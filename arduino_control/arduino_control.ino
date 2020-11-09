86dcb3b298c1d7745825a4165497dbee6d869dd1
Husky <husky@shanghaitech.edu.cn>
2020-11-09 16:39:05 +0800

    zzw

--- arduino_control/arduino_control.ino
+++ arduino_control/arduino_control.ino
@@ -1,3 +1,16 @@
+#include <ArduinoHardware.h>
+#include <ros.h>
+#include <std_msgs/String.h>
+#include <std_msgs/Bool.h>
+
+ros::NodeHandle nh;
+std_msgs::String status_msg;
+ros::Publisher status_publisher("suction_status", &status_msg);
+void suction_action(const std_msgs::Bool& cmd);
+ros::Subscriber<std_msgs::Bool> cmd_subscriber("suction_cmd", &suction_action);
+// Global boolean variable
+bool controller = false;
+
 
 // Pin 13 is the indicator of running, as it is a LED on board.
 int led = 13;
@@ -12,6 +25,10 @@ void setup() {
   pinMode(led, OUTPUT);     
   pinMode(switch_motor, OUTPUT);
   pinMode(switch_reverse, OUTPUT);
+
+  // initialize ros
+  nh.initNode();
+  nh.subscribe(cmd_subscriber);
 }
 
 
@@ -20,6 +37,7 @@ void loop() {
   // If get message from ROS
   // Then do something
   // Now independentent, halt for 1 second after switching
+  if (controller){
   digitalWrite(led, HIGH);  
   motor_on();
   delay(1000);
@@ -30,6 +48,10 @@ void loop() {
   motor_off();
   digitalWrite(led, LOW);
   delay(1000);
+  }else{
+    
+  }
+  nh.spinOnce();
 }
 
 
@@ -52,4 +74,13 @@ void reverse_on(){
 // Rotate normally.
 void reverse_off(){
   digitalWrite(switch_reverse,LOW);
+}
+
+// main entrance recieving ROS topic
+void suction_action(const std_msgs::Bool& cmd) {
+  if (cmd.data){
+    controller = true;
+  } else{
+    controller = false;
+    }
 }
